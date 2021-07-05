5dc70ff9092efb6e3fc6118ffdea00d4939a00a0
Rasl2000 <80060356+Rasl2000@users.noreply.github.com>
2021-07-05 17:26:50 -0500

    Prueba2

---
+++ frontend/src/screens/ShippingAddressScreen.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { saveShippingAddress } from '../actions/cartActions';
+import CheckoutSteps from '../components/CheckoutSteps';
+
+export default function ShippingAddressScreen(props) {
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  if (!userInfo) {
+    props.history.push('/signin');
+  }
+  const [fullName, setFullName] = useState(shippingAddress.fullName);
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      saveShippingAddress({ fullName, address, city, postalCode, country })
+    );
+    props.history.push('/payment');
+  };
+  return (
+    <div>
+      <CheckoutSteps step1 step2></CheckoutSteps>
+      <form className="form" onSubmit={submitHandler}>
+        <div>
+          <h1>Direccion de Envio</h1>
+        </div>
+        <div>
+          <label htmlFor="fullName">Nombre completo</label>
+          <input
+            type="text"
+            id="fullName"
+            placeholder="Introduzca su nombre completo"
+            value={fullName}
+            onChange={(e) => setFullName(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="address">Direccion</label>
+          <input
+            type="text"
+            id="address"
+            placeholder="Introduce la direccion"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="city">Ciudad</label>
+          <input
+            type="text"
+            id="city"
+            placeholder="Introduza la ciudad"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="postalCode">Codigo Postal</label>
+          <input
+            type="text"
+            id="postalCode"
+            placeholder="Introduza el codigo postal"
+            value={postalCode}
+            onChange={(e) => setPostalCode(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label htmlFor="country">Pais</label>
+          <input
+            type="text"
+            id="country"
+            placeholder="Introduza el pais"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            required
+          ></input>
+        </div>
+        <div>
+          <label />
+          <button className="primary" type="submit">
+            Continuar
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
